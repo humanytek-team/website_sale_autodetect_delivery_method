f9baaee2c735b09cf3b7dc952c5889433466ad97
Manuel Marquez <manuel@humanytek.com>
2018-01-22 21:38:29 -0500

    Work In Progress

---
+++ __openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Humanytek - Manuel Marquez <manuel@humanytek.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': '',
+    'version': '9.0.0.1.0',
+    'category': 'Sales',
+    'author': 'Humanytek',
+    'website': "http://www.humanytek.com",
+    'license': 'AGPL-3',
+    'depends': [
+
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False
+}
